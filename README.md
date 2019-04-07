147b180274599e1071cc8110374198fe6e738aba
DKrivets <Dmitry.Krivets@gmail.com>
2019-04-07 10:27:41 +0300

    Rename files

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-## Text Buffer for Emacs
+## Named Buffer for Emacs
 
-TEXT-BUFFER mode is a small extension which has a
+NAMED-BUFFER mode is a small extension which has a
 simple way to create new buffer does not think about it name.
 By default, name of a new buffer looks like TEMP<2>, but you can set your name.
 When the buffer will be created it name will be \"TEMP\". The next 
 buffer name increment number \"TEMP<2>\" and etc.
-![Emacs ui: text-buffer in use.](https://github.com/dkrivets/text-buffer/blob/assets/emacs_textbufer.png?raw=true)
+![Emacs ui: named-buffer in use.](https://github.com/dkrivets/named-buffer/blob/assets/emacs_textbufer.png?raw=true)
 
 ## Customization
 Have 1 customize parameters:
-1. TEXT-PREFIX-NAME: Prefix of buffer name - "TEMP".
+1. NAMED-BUFFER-PREFIX-NAME: Prefix of buffer name - "TEMP".
 
 ## Installation
 ```emacs-lisp
-(require 'text-buffer)
-(text-buffer)
-(global-set-key (kbd "C-x n") #'text-buffer-create)
+(require 'named-buffer)
+(named-buffer)
+(global-set-key (kbd "C-x n") #'named-buffer-create)
 ```
--- named-buffer.el
+++ named-buffer.el
@@ -1,16 +1,16 @@
-;;; text-buffer.el --- Creating named buffer -*- coding: utf-8; lexical-binding: t -*-
+;;; named-buffer.el --- Creating named buffer -*- coding: utf-8; lexical-binding: t -*-
 
 ;; Author: DKrivets
 ;; Created: 29 Dec 2018
-;; Version: 0.0.4
-;; Keywords: text-buffer, languages, programming
-;; Homepage: https://github.com/dkrivets/text-buffer
+;; Version: 0.0.5
+;; Keywords: named-buffer, languages, programming
+;; Homepage: https://github.com/dkrivets/named-buffer
 ;; Package-Require: ((emacs "24"))
 
 ;;; Commentary:
 ;;  Simple way to create new buffer does not think about it name.
 ;;  Have 1 parameter1:
-;;  1. TEXT-PREFIX-NAME: Prefix of buffer name.
+;;  1. NAMED-BUFFER-PREFIX-NAME: Prefix of buffer name.
 ;;  By default, it looks like "TEMP".
 ;;  When buffer will be created it name will be "TEMP".
 ;;  And the next buffer will have a name like "TEMP<2>".
@@ -19,7 +19,8 @@
 ;; Change log:
 ;; 09 Feb 2019 Deleted text-buffer-load-hook and default key binding
 ;; 11 Jan 2019 Simplify algorithm: idea from alphapapa
-;; 10 Jan 2019 Added hook \\[text-buffer-load-hook] it may help to change key binding
+;; 10 Jan 2019 Added hook \\[named-buffer-load-hook] it may help to change key binding
+;; 07 Apr 2019 Rename mode and file
 ;;; Code:
 
 ;;;; Requirements
@@ -27,72 +28,72 @@
 
 ;;;; Customization
 
-(defgroup text-buffer nil
+(defgroup named-buffer nil
   "Simple way to create new buffer does not think about it name."
   :group 'applications)
 
 
-(defcustom text-buffer-prefix-name "TEMP"
+(defcustom named-buffer-prefix-name "TEMP"
   "Prefix of buffer name."
   :type  'string
-  :group 'text-buffer)
+  :group 'named-buffer)
 
 
 ;;;; Functions
 
-(defun text-buffer--base-create-buffer (name)
+(defun named-buffer--base-create-buffer (name)
   "Create buffer with NAME and switch to it."
   (switch-to-buffer (get-buffer-create name)))
 
 
-(defun text-buffer--get-name (name)
+(defun named-buffer--get-name (name)
   "Get NAME of buffer.
-If NAME is null generate new name base on `text-buffer-prefix-name'."
+If NAME is null generate new name base on `named-buffer-prefix-name'."
   (if (> 0 (length name))
       name
-    (generate-new-buffer-name text-buffer-prefix-name)))
+    (generate-new-buffer-name named-buffer-prefix-name)))
 
 ;;;;; Commands
 
 ;;;###autoload
-(defun text-buffer-create ()
+(defun named-buffer-create ()
   "Create buffer with NAME interactivly.
 Main function which creates buffer with name you can input or default
 which count from exist buffer."
   (interactive)
   ;; Create user helper with buffer-name
-  (let* ((desc (format "New buffer name:[%s] " (generate-new-buffer-name text-buffer-prefix-name)))
+  (let* ((desc (format "New buffer name:[%s] " (generate-new-buffer-name named-buffer-prefix-name)))
          (name (read-string desc))  ;; Read user data from mini-buffer
-         (buf-name (text-buffer--get-name name)))
+         (buf-name (named-buffer--get-name name)))
 	;; Run process
-	(text-buffer--base-create-buffer buf-name)))
+	(named-buffer--base-create-buffer buf-name)))
 
 
 ;;;###autoload
-(define-minor-mode text-buffer
-  "TEXT-BUFFER mode.
+(define-minor-mode named-buffer
+  "NAMED-BUFFER mode.
 Simple way to create new buffer does not think about it name.
 Have 1 customize parameters:
-1. TEXT-PREFIX-NAME: Prefix of buffer name.
+1. NAMED-BUFFER-PREFIX-NAME: Prefix of buffer name.
 By default, it looks like \"TEMP\".
 When the buffer will be created it name will be \"TEMP\" and next \"TEMP<2>\".
 
 Example:
-(require 'text-buffer)
-(text-buffer)
-(global-set-key (kbd \"C-x n\") #'text-buffer-create)
+(require 'named-buffer)
+(named-buffer)
+(global-set-key (kbd \"C-x n n\") #'named-buffer-create)
 "
-  :group 'text-buffer
-  :require 'text-buffer
-  :lighter " TB"
+  :group 'named-buffer
+  :require 'named-buffer
+  :lighter " NB"
   :global t
-  (make-local-variable 'text-buffer-map)
+  (make-local-variable 'named-buffer-map)
 )
 
 ;;;; Footer
 
-(provide 'text-buffer)
+(provide 'named-buffer)
 ;; Local Variables:
 ;; indent-tabs-mode: nil
 ;; End:
-;;; text-buffer.el ends here
+;;; named-buffer.el ends here
